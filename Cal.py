95744ab27f25171947d4d2138c41375c4af6a7b8
raghavkovvuri-w <raghav.kovvuri@ieg.ac.uk>
2024-10-13 21:38:41 +0100

    Calculations (Add and Substraction)

--- Cal.py
+++ Cal.py
@@ -6,16 +6,6 @@ def subtract(a, b):
     """Subtract b from a and return the result."""
     return a - b
 
-def multiply(a, b):
-
-    return a * b
-
-def div(a,b):
-
-    return a / b
-
 # Test our functions
 print(f"5 + 3 = {add(5, 3)}")
 print(f"10 - 4 = {subtract(10, 4)}")
-print(f'2 * 5 = {multiply(2,5)}')
-print(f'10 / 2 = {div(10,2)}')
